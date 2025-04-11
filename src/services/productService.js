50d1435c8d9ea4771f2c80a78817a766f843917b
Muhammad Zulqarnain <m.nain471@gmail.com>
2025-04-11 08:19:57 +0500

    env updated

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,26 +1,41 @@
 export async function getProductList(searchTerm){
-    const response = await fetch(`${process.env.REACT_APP_HOST}/444/products?name_like=${searchTerm ? searchTerm : ""}`);
-    if(!response.ok){
-        throw { message: response.statusText, status: response.status }; //eslint-disable-line
+    try{
+        const response = await fetch(`${process.env.REACT_APP_HOST}/444/products?name_like=${searchTerm ? searchTerm : ""}`);
+        if(!response.ok){
+            throw { message: response.statusText, status: response.status }; //eslint-disable-line
+        }
+        const data = await response.json()
+        return data;
+        
+    } catch(err){
+        console.error("Fetch failed:", err.message || err);
     }
-    const data = await response.json()
-    return data;
 }
 
 export async function getProduct(id){
-    const response = await fetch(`${process.env.REACT_APP_HOST}/444/products/${id}`);
-    if(!response.ok){
-        throw { message: response.statusText, status: response.status }; //eslint-disable-line
+    try{
+        const response = await fetch(`${process.env.REACT_APP_HOST}/444/products/${id}`);
+        if(!response.ok){
+            throw { message: response.statusText, status: response.status }; //eslint-disable-line
+        }
+        const data = await response.json()
+        return data;
+
+    } catch(err){
+        console.error("Fetch failed:", err.message || err);
     }
-    const data = await response.json()
-    return data;
 }
 
 export async function getFeaturedList(){
-    const response = await fetch(`${process.env.REACT_APP_HOST}/444/featured_products`);
-    if(!response.ok){
-        throw { message: response.statusText, status: response.status }; //eslint-disable-line
+    try{
+        const response = await fetch(`${process.env.REACT_APP_HOST}/444/featured_products`);
+        if(!response.ok){
+            throw { message: response.statusText, status: response.status }; //eslint-disable-line
+        }
+        const data = await response.json()
+        return data;
+
+    } catch(err){
+        console.error("Fetch failed:", err.message || err);
     }
-    const data = await response.json()
-    return data;
 }
